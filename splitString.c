185277c60e517f1a4c834784bfa9f471461f8dc3
isaac-ik <isaac.ikpemi@gmail.com>
2023-11-29 15:51:05 -0800

    Split a string: correction to a segmentation fault

--- splitString.c
+++ splitString.c
@@ -12,11 +12,19 @@
 int splitStr(char *str)
 {
 	char *sep = " ";
+	char *s;
 	
-	while (str != NULL)
+	if (str == NULL)
 	{
-		str = strtok(str, sep);
-		printf("%s\n", str);
+		return (-1);
+	}
+
+	s = strtok(str, sep);
+	
+	while (s != NULL)
+	{
+		printf("%s\n", s);
+		s = strtok(NULL, sep);
 	}
 	return (0);
 }
@@ -29,13 +37,18 @@ int splitStr(char *str)
  */
 void main(int argc, char **argv)
 {
-	int i = 1;
+	int i = 0;
+	int result;
 
 	/* parse the argument vector */
 	while (argv[i] != NULL)
 	{
-		printf("the argument number%i is %s\n", i, argv[i]);
-		splitStr(argv[i]);
+		printf("the argument number %i is %s\n", i, argv[i]);
+		result = splitStr(argv[i]);
+		if (result == -1)
+		{
+			printf(" unable to split\n");
+		}
 		i++;
 	}
 }
