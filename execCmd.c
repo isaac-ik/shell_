65f625674a14476d87a37e8a15e348b10b683337
isaac-ik <isaac.ikpemi@gmail.com>
2023-12-01 06:54:31 -0800

    fork, execve and wait

---
+++ execCmd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	pid_t pid;
+	int i;
+	char **args = {};
+	char **envp = { NULL };
+
+	if (argc )	
+	/* create a loop */
+}
